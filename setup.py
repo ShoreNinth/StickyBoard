62d057d331ecd5f764574210363e15b0d13af202
Shore9th <54047776+ShoreNinth@users.noreply.github.com>
2024-01-24 23:04:52 +0800

    完善setup.py

--- setup.py
+++ setup.py
@@ -11,14 +11,23 @@ buildForWindows = "pyinstaller -F -w -i icon.ico main.py"
 print("1.为Windows构建\n"+
       "2.为Gnome Linux构建\n"
       +"3.为KDE Linux构建\n")
+promptList=[1,2,3]
 
-prompt = int(input('请输入序号：'))
+loopLock = 1
 
-if prompt == 1:
-    os.system(buildForWindows)
-elif prompt == 2:
-    print("敬请期待")
-    pass
-elif prompt == 3:
-    print("敬请期待")
-    pass
+while(loopLock==1):
+    prompt = int(input('请输入序号：'))
+
+    if prompt in promptList:
+        loopLock=0
+    else:
+        print("输入序号错误，请重新输入。")
+
+    if prompt == 1:
+        os.system(buildForWindows)
+    elif prompt == 2:
+        print("敬请期待")
+        pass
+    elif prompt == 3:
+        print("敬请期待")
+        pass
